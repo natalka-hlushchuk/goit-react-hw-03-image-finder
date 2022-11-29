ba53a23490a728c0c5eb7aab8ff3a9ecff74572d
Natalka Hlushchuk <hlushchuk.nataliia@gmail.com>
2022-11-29 18:03:03 +0100

    fix

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -39,7 +39,7 @@ class App extends Component {
   }
 
   handelFormSubmit = photoName => {
-    this.setState({ photoName, page: 1 });
+    this.setState({ photoName, page: 1, photos: [] });
   };
   onClickModal = url => {
     this.setState({ largePhoto: url });
